32cf1e378fe1d8b8f219e8fb3a4561412b8a4744
ibraheem Hasan <rules656@gmail.com>
2019-12-11 16:50:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,4 @@ releasePressStatement(){
 return `You will see great things from Scuber`;}
 sayHi(){
 return `Hi, my name is ${this.name}. I am from ${this.homeState},
-and I was trained in ${this.training}`
-}
+and I was trained in ${this.training}`;}
